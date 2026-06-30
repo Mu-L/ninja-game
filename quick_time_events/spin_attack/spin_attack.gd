bbc23be655cae8a43a31305468496862e4e4e7b2
buzzbuzz20xx <samirskyland@gmail.com>
2026-06-30 12:50:59 +0300

    Reworked spin attack QTE

--- quick_time_events/spin_attack/spin_attack.gd
+++ quick_time_events/spin_attack/spin_attack.gd
@@ -5,8 +5,10 @@ extends QuickTimeEvent
 @onready var slash_effect: AnimatedSprite2D = %SlashEffect
 @onready var slash_sound: AudioStreamPlayer = %SlashSound
 @onready var instructions_label: RichTextLabel = %InstructionsLabel
+@onready var timer: Timer = %Timer
+@onready var timer_label: Label = %TimerLabel
 
-var actions: PackedStringArray = ["move up", "move right", "move down", "move left"]
+var actions: PackedStringArray = ["move right", "move left"]
 var i := 0
 
 func _ready() -> void:
@@ -15,15 +17,18 @@ func _ready() -> void:
 func start(ally: AllyBattler) -> void:
 	await ally.move_to(ally.get_main_target_battler().global_position - Vector2(25,0))
 	super.start(ally)
+	timer.start()
 
 func _process(_delta: float) -> void:
 	if not started:
 		return
+	timer_label.text = "%.2f" % timer.time_left
 	if Input.is_action_just_pressed(actions[i % len(actions)]):
-		i += 5
+		i += 3
 		var tween := create_tween()
 		tween.tween_property(progress_bar, "value", i, 0.15)
 	if progress_bar.value == progress_bar.max_value:
+		timer.stop()
 		started = false
 		canvas_layer.hide()
 		slash_effect.show()
@@ -41,3 +46,9 @@ func _process(_delta: float) -> void:
 		await get_tree().create_timer(0.1).timeout
 		await ally.move_to(ally_starting_pos)
 		finished.emit()
+
+func _on_timer_timeout() -> void:
+	started = false
+	target.show_stat_bars()
+	await ally.missed_effect(target.global_position)
+	finished.emit()
